67d36e01292a297b23ce96c2f2da5c1cbcca3f64
bishoksan <bishoksan@gmail.com>
2016-02-08 18:06:51 +0100

    some internal files removed

--- src/logen/gxmodules.pl
+++
@@ -1,2 +0,0 @@
-%% these imports are added to all gx files
-:- use_module(library(terms_vars)).
--- src/logen/op_decl.pl
+++
@@ -1,7 +0,0 @@
-%:- op(1150,fx,user:(filter)).
-%:- op(1150,fx,user:(type)).
-%:- op(500,xfx,user:(--->)).
-
-:- op(1150,fx,filter).
-:- op(1150,fx,type).
-:- op(500,xfx,--->).
